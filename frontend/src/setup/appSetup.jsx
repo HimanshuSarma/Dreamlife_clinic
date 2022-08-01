033f75f0dab1fa07fb2a777aecacaead665c1a0f
HimanshuSarma <sweetiehimanshu@gmail.com>
2022-08-01 15:46:33 +0530

    first

--- frontend/src/setup/appSetup.jsx
+++ frontend/src/setup/appSetup.jsx
@@ -2,6 +2,7 @@ let base_url;
 
 let domain = document.domain;
 
-if(domain === 'localhost') base_url = 'http://localhost:5000';
+if(domain === 'localhost') base_url = 'http://localhost:5000' 
+else base_url = domain;
 
 export default base_url;
